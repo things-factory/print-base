6702384e1b9b6fc9186f6c3430682b95681ca356
leemyunggyu <jaylee@hatiolab.com>
2019-05-02 17:15:59 +0800

    v0.0.2

--- src/reducers/print.js
+++ src/reducers/print.js
@@ -1,13 +1,14 @@
-import { UPDATE_PRINT_BASE } from '../actions/print'
+import { TOGGLE_PRINT_LIST } from '../actions/print'
 
 const INITIAL_STATE = {
+  show: false,
   prints: ['HP DesignJet 60 Z6610', 'CANON IR ADV C5550i II', 'EPSON aculaser-C9300N A3']
 }
 
 const main = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case UPDATE_PRINT_BASE:
-      return { ...state }
+    case TOGGLE_PRINT_LIST:
+      return { ...state, show: !state.show }
 
     default:
       return state

f09568faa76b0ff0439a4be911797980110555a5
Hearty Oh <heartyoh@hatiolab.com>
2019-10-19 17:45:07 +0900

    Update preview-printer-handler.js

--- client/handlers/preview-printer-handler.js
+++ client/handlers/preview-printer-handler.js
@@ -1,21 +1,3 @@
-function content_print(element) {
-  if (element) {
-    var restoreStyle = element.style
-
-    element.style.position = 'fixed'
-    element.style.left = 0
-    element.style.top = 0
-    element.style.width = '100vw'
-    element.style.height = '100vh'
-
-    window.print()
-
-    element.style = restoreStyle
-  } else {
-    window.print()
-  }
-}
-
 export async function previewPrinterHandler(printer, { accept, content, name, options }) {
   /* content should be a element */
   const element = typeof content == 'function' ? content.call() : content
@@ -25,7 +7,7 @@ export async function previewPrinterHandler(printer, { accept, content, name, op
     await element.updateComplete
   }
 
-  await content_print(element)
+  window.print()
 
   if (element && element.updateContext) {
     element.updateContext()
